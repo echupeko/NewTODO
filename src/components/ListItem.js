5ef5725272f5d4f10797354a8322d80188822fe7
Dell <echupeko@gmail.com>
2019-06-17 23:33:58 +0700

    last update

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,13 +3,17 @@ import '../style/ListItem.css';
 
 class ListItem extends React.Component {
 
+    state = {
+        checkTask: this.props.item.checkTask
+    }
     checkedTask = () => {
+        this.setState({checkTask: !this.state.checkTask});
         this.props.checkingTask(this.props.item.idTask);
     }
     render() {
         return (
             <div className={'itemRow'}>
-                <span><input type={'checkbox'} onChange={this.checkedTask} checked={this.props.item.checkTask}></input></span>
+                <span><input type={'checkbox'} onChange={this.checkedTask} checked={this.state.checkTask}></input></span>
                 <span>{this.props.item.titleTask}</span>
                 <span>{this.props.item.descriptionTask}</span>
                 <span>{this.props.item.importanceTask}</span>
